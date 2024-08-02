48c6aeda9cd8be6c659925e49398fec37a4f232d
Jon_Em <45055471+Jmz0127@users.noreply.github.com>
2024-08-02 14:19:24 -0400

    added pyplot functionality to visualize select income statement items

--- main.py
+++ main.py
@@ -6,11 +6,32 @@ import matplotlib.pyplot as plt
 api_key = 'IHs6ny612Q0HxcgjsQBlUwfW7bMX27EF'
 
 #add the ticker of the company's income statement you would like to view
-company = 'FB'
+company = 'AAPL'
 #add the amount of years of income statement data you would like to view
-years = 2
+years = 10
+
 
 income_statement = requests.get(f'https://financialmodelingprep.com/api/v3/income-statement/{company}?limit={years}&apikey={api_key}')
 income_statement = income_statement.json()
 
-print(income_statement)
+
+# uncomment line directly below to pull all income statement lines
+# print(income_statement)
+
+# uncomment line directly below if you want an individual line item from the income statement printed. here is an example of a revenue comparison. the number 0 represents the last full year. in this case it is 2023's revenue.
+# print(income_statement[0]['revenue'])
+
+# some other examples or items you can pull
+# print(income_statement[0]['grossProfit'])
+# print(income_statement[0]['eps'])
+# print(income_statement[0]['ebitda'])
+
+revenues = list(reversed([income_statement[i]['revenue'] for i in range(len(income_statement))]))
+profits = list(reversed([income_statement[i]['grossProfit'] for i in range(len(income_statement))]))
+
+plt.plot(revenues, label='Revenue')
+plt.plot(profits, label='Profit')
+plt.title('Revenue & Profit')
+plt.legend(loc = 'upper right')
+plt.show()
+
